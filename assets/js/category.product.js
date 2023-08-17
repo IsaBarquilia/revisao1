cd26afb3a6547704314422c6533f4682b59d6238
IsaBarquilia <isabelle.barquilia.564@gmail.com>
2023-08-17 10:31:29 -0300

    feat: create class CategoryService and addCategory

--- assets/js/category.product.js
+++ assets/js/category.product.js
@@ -1,14 +1,27 @@
 class Category{
     constructor(name, id){
-        this.name = name
-        this.id = id
-        this.products = []
+        this.name = name;
+        this.id = id;
+        this.products = [];
     }
 }
 class Products{
     constructor(id, name, price){
-        this.name = name
-        this.id = id
-        this.price = price
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.category = category;
+    }
+}
+class CategoryService{
+    constructor(){
+        this.categories = [];
+        this.nextCategoryId = 0;
+    }
+
+    addCategory(name){
+        const id = this.nextCategoryId ++;
+        const category = new Category(id, name);
+        this.categories.push(category);
     }
 }
